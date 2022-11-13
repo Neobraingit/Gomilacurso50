ca0633ed86adeeb986b9613571b14ec033196669
Neobraingit <Neobraingit@github.com>
2022-11-13 13:10:42 +0100

    Strings

---
+++ Format, saltos de línea y tabulaciones.py
@@ -0,0 +1,18 @@
+# Función str()
+nombre = 'María'
+edad = 22
+print (f'Mi hermana se llama {nombre} y tiene', str(edad), 'años')
+
+
+
+
+# Format ()
+var = 'Marcos '
+var2 = 'Carmona '
+var3 = 'García'
+print (f'Mi nomre es ', {var}, 'y mi apellido es ', {var2}, 'y mi segundo apellido es ', {var3})
+
+# Substrings
+cadena = 'Mi nombre es Marcos Carmona García'
+print (cadena[0:3])
+print (cadena[::-1])
