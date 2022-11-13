a849dba2dc8399898ce2066c3baf05c5a138cb76
Neobraingit <Neobraingit@github.com>
2022-11-13 12:52:51 +0100

    Strings

--- Strings.py
+++ Strings.py
@@ -1,6 +1,10 @@
 '''Es una secuencia ordenada de letra o números que van entre comillasd'''
-s1 = 'Esto es un string'
+s1 = 'Esto es un string '
 s2 = '123455'
+
+# Contatenación de strings
+resultado = s1 + s2
+print (resultado) # Como podemos ver nos los suma, si no que los une
 
 
 
