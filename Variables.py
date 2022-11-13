9ee646027d90bd8ada6669f6addabcdd03bcb79b
Neobraingit <Neobraingit@github.com>
2022-11-13 12:02:19 +0100

    Variables

---
+++ Variables.py
@@ -0,0 +1,24 @@
+'''Variables son un espacio en el sistema de almacenaje (memoria Ram)'''
+var = 1
+print (var)
+var2 = 2
+print (var2)
+print (var + var2)
+
+'''Restricciones sobre los nombres de las variables:
+   No pueden empezar por caracteres especiales, tampoco contenerlos
+   No pueden empezar por números
+   No pueden ser llamadas igual que las palabras reservadas en Python
+   No pueden contener espacios'''
+
+''' Las variables son camelcase, pascalcase, snake_case, kebab_case'''
+camelcase: nombreMascota
+pacalcase: NombreMascota
+snake_case: Nombre_mascota
+kebab-case: Nombre-mascota
+
+'''Declarando múltiples variables en una sola línea'''
+nombre, apellido, mote = 'Marcos', 'Carmona', 'Cuca'
+print (nombre)
+print (apellido)
+print (mote)
