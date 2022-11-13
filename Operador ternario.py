2d99a072a56d414ec66d77cbe26ba3b128d9e4e9
Neobraingit <Neobraingit@github.com>
2022-11-13 17:15:53 +0100

    Operación ternario

---
+++ Operador ternario.py
@@ -0,0 +1,6 @@
+'''Es usar el if y el elif y el else en una sola línea'''
+edad = 20
+texto_mayor = 'Eres mayor de edad en España'
+Texto_menor = 'Eres menor de edad e España'
+
+print (texto_mayor) if edad >= 18 else print (texto_menor)
