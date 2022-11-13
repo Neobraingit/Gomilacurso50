413d425ae202a8aeb121e2525add52af553032fa
Neobraingit <Neobraingit@github.com>
2022-11-13 12:37:24 +0100

    Númneros

--- Import.py
+++ Import.py
@@ -1,4 +1,4 @@
 '''Se utiliza para cargar librerías o módulos'''
-import math
+import math # Módulo relacionado con las matemáticas
 from math import pi
 print (pi)
---
+++ Números.py
@@ -0,0 +1,37 @@
+''' int : número entero
+    float : número con decimales'''
+
+# Para saber que tipo de número es usamos type()
+num = 12
+print (type(num))
+
+# Los números flotantes llevan los decimales depués de un cero
+num2 = 12.345
+print (type(num2))
+
+# Casting de números
+num3 = int(num2)
+print (type(num3))
+
+# Operaciones aritméticas
+''' Para sumar dos números usamos:
+    +
+    para multiplicar usamos:
+    *
+    para restar usamos:
+    -
+    para dividir usamos :
+    /
+    para las potencias se usa:
+    **
+    '''
+# Si existe en la operación hay un float, el resultado también será un float
+
+# Orden de las operaciones matemáticas
+'''Primero lo que se haya entre paréntesis
+   Después, las potencias
+   Después, productos y divisiones
+   Finalmente, sumas y restas'''
+
+
+   '''
