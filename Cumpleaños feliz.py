f608c7e009db03516135a0f28ce8271e99f7dbfa
Neobraingit <Neobraingit@github.com>
2022-11-13 12:59:46 +0100

    Cumpleaños feliz

---
+++ Cumpleaños feliz.py
@@ -0,0 +1,4 @@
+cancion1 = 'Cuampleaños feliz...'
+cancion2 = 'Te deseamos todos...'
+
+print (cancion1 * 2 + cancion2 + cancion1 )
---
+++ Función print.py
@@ -0,0 +1,3 @@
+# Muestra por pantalla el mensaje que quedamos o la operación que deseemos
+
+print ('Hola mundo')
