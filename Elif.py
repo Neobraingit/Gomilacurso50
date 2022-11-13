fbc53281c2e02ca37f62d208a64bf626886bb9f7
Neobraingit <Neobraingit@github.com>
2022-11-13 17:11:37 +0100

    Elif

---
+++ Elif.py
@@ -0,0 +1,5 @@
+texto = input('Introduce un texto que contenga un nombre: ')
+if ' ' in texto:
+    print ('Me llamo Marcos...')
+elif ' ' not in texto:
+    print ('No me llamo Marcos')
---
+++ Else.py
@@ -0,0 +1,5 @@
+texto = input('Introduce un texto lo más largo posible: ')
+if len(texto) == 30:
+    print ('Tiene los caracteres correctos')
+else:
+    print ('No tiene esos caracteres')
