8955cbc4d963a4b17603dc96171d920eb8eb049c
Neobraingit <Neobraingit@github.com>
2022-11-16 20:20:01 +0100

    Funciones

--- Funciones.py
+++ Funciones.py
@@ -6,3 +6,19 @@ def mi_función():
 
 
 mi_función()
+
+def conparametros(nombre):
+    print (f'Buenos días {nombre}')
+
+conparametros('MARCOS')
+
+def contadordeletras():
+    string = input('Introduce un string: ')
+    contador = 0
+    vocales = 'aeiou'
+    for i in string:
+        if i in vocalesa:
+            contador += 1
+    print (contador)
+
+contadordeletras()
